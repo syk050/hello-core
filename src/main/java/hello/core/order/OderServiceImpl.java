862898c90469de956d5685fd8ceb825481efce69
syk050 <syk050@naver.com>
2022-10-21 12:05:24 +0900

    기능 수정시 OCP 위반

--- src/main/java/hello/core/order/OderServiceImpl.java
+++ src/main/java/hello/core/order/OderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,10 @@ import hello.core.member.MemoryMemberRepository;
 public class OderServiceImpl implements OderService {
 
     private final MemberRepository repository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // PDF 3- 4p
+    //OderServiceImpl 클라이언트 코드가 수정 => OCP 위반
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Oder createOrder(Long memberId, String itemName, int itemPrice) {
