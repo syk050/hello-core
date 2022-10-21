5f03d3fb87904c665afd3d1e3843bcad8977ab94
syk050 <syk050@naver.com>
2022-10-21 10:51:01 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+    // 동시성 이슈 문제로 concurrent hashmap을 사용해야 하지만
+    // 간단한 예제이기 떄문에 HashMap을 사용
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+}
