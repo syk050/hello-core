0dfd4cf9f12861c896c66823fc4b7500f0710f5a
syk050 <syk050@naver.com>
2022-10-21 11:00:46 +0900

    DIP 위반

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    // 실제 할당하는부분이 구현체를 의존함
+    // 추상화(인터페이스)에도 의존하고 구체화에도 의존하게 됨 => DIP 위반
     private final MemberRepository repository = new MemoryMemberRepository();
 
     @Override
