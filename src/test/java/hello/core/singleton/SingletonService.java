39b0184f66657d904fa4dc490abaa508ed9a249f
syk050 <syk050@naver.com>
2022-10-22 11:53:48 +0900

    싱글톤 패턴

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,27 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    // static 영역에 객체 instance를 미리 하나 생성해서 올려둔다
+    private static final SingletonService instance = new SingletonService();
+
+    // 이 객체 인스턴스가 필요하면 오직 getInstance() 메서드를 통해서만 조회할 수 있다.
+    // 이 메서드를 호출하면 항상 같은 인스턴스를 반환한다.
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    private SingletonService() {
+        // 딱 1개의 객체 인스턴스만 존재해야 하므로,
+        // 생성자를 private으로 막아서
+        // 혹시라도 외부에서 new 키워드로 객체 인스턴스가 생성되는 것을 막는다.
+    }
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -21,4 +21,19 @@ public class SingletonTest {
 
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+//        System.out.println("singletonService1: " + singletonService1);
+//        System.out.println("singletonService2: " + singletonService2);
+
+        // same ==
+        // equal
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+
+    }
 }
