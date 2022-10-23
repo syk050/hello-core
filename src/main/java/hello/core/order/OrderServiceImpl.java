bd6825d352f0f6a90f61f2911545ad7fa56ce4e4
syk050 <syk050@naver.com>
2022-10-23 14:30:10 +0900

    lombok 적용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // 면 final이 붙은 필드를 모아서 생성자를 자동으로 만들어준
 public class OrderServiceImpl implements OrderService {
 
     // final: 생상자나 초기값을 통해서만 수정이 가능하다
@@ -22,24 +24,24 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository repository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository repository, DiscountPolicy discountPolicy) {
-        /* 생성자 주입의 이점
-         * 객체를 생성할 때 딱 1번만 호출되므로 이후에 호출되는 일이 없다.
-         * 따라서 불변하게 설계할 수 있다.
-         *
-         * 생성자 주입을 사용하면 주입 데이터를 누락 했을 때 컴파일 오류가 발생한다
-         *
-         * final 키워드
-         * 생성자 주입을 사용하면 필드에 final 키워드를 사용할 수 있다
-         * 생성자에서 혹시라도 값이 설정되지 않는 오류를 컴파일 시점에 막아준다
-         * 기억하자! 컴파일 오류는 세상에서 가장 빠르고, 좋은 오류다
-         * 수정자 주입을 포함한 나머지 주입 방식은 모두 생성자 이후에 호출되므로,
-         * 필드에 final 키워드를 사용할 수 없다.
-         */
-        this.repository = repository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository repository, DiscountPolicy discountPolicy) {
+//        /* 생성자 주입의 이점
+//         * 객체를 생성할 때 딱 1번만 호출되므로 이후에 호출되는 일이 없다.
+//         * 따라서 불변하게 설계할 수 있다.
+//         *
+//         * 생성자 주입을 사용하면 주입 데이터를 누락 했을 때 컴파일 오류가 발생한다
+//         *
+//         * final 키워드
+//         * 생성자 주입을 사용하면 필드에 final 키워드를 사용할 수 있다
+//         * 생성자에서 혹시라도 값이 설정되지 않는 오류를 컴파일 시점에 막아준다
+//         * 기억하자! 컴파일 오류는 세상에서 가장 빠르고, 좋은 오류다
+//         * 수정자 주입을 포함한 나머지 주입 방식은 모두 생성자 이후에 호출되므로,
+//         * 필드에 final 키워드를 사용할 수 없다.
+//         */
+//        this.repository = repository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
