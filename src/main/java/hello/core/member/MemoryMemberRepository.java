3005eee7229c31e4173913dc019b7dec4f456e01
syk050 <syk050@naver.com>
2022-10-23 13:41:03 +0900

    AutoAppConfig와 AppConfig에서 MemberRepository 타입 중복 등록

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,11 +1,13 @@
 package hello.core.member;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@Primary
 public class MemoryMemberRepository implements MemberRepository {
     // 동시성 이슈 문제로 concurrent hashmap을 사용해야 하지만
     // 간단한 예제이기 떄문에 HashMap을 사용
