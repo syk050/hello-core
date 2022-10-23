94970b0b4f681bf30b0a48984395ddedf5204da2
syk050 <syk050@naver.com>
2022-10-23 14:06:47 +0900

    생성자 주입의 이점 설명

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    // final: 생상자나 초기값을 통해서만 수정이 가능하다
+    // 초기에 값이 세팅이 되지않으면 오류가 발생한다
 //    private final MemberRepository repository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
@@ -18,10 +20,23 @@ public class OrderServiceImpl implements OrderService {
     // 누군가가 클라이언트인 OrderServiceImpl 에 DiscountPolicy 의 구현 객체를
     // 대신 생성하고 주입해주어야 한다 => AppConfig
     private final MemberRepository repository;
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
 
     @Autowired
     public OrderServiceImpl(MemberRepository repository, DiscountPolicy discountPolicy) {
+        /* 생성자 주입의 이점
+         * 객체를 생성할 때 딱 1번만 호출되므로 이후에 호출되는 일이 없다.
+         * 따라서 불변하게 설계할 수 있다.
+         *
+         * 생성자 주입을 사용하면 주입 데이터를 누락 했을 때 컴파일 오류가 발생한다
+         *
+         * final 키워드
+         * 생성자 주입을 사용하면 필드에 final 키워드를 사용할 수 있다
+         * 생성자에서 혹시라도 값이 설정되지 않는 오류를 컴파일 시점에 막아준다
+         * 기억하자! 컴파일 오류는 세상에서 가장 빠르고, 좋은 오류다
+         * 수정자 주입을 포함한 나머지 주입 방식은 모두 생성자 이후에 호출되므로,
+         * 필드에 final 키워드를 사용할 수 없다.
+         */
         this.repository = repository;
         this.discountPolicy = discountPolicy;
     }
