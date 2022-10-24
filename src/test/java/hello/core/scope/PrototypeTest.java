7afa82bcdc24f20b86813ceaa6a94d9b8a3be99e
syk050 <syk050@naver.com>
2022-10-24 13:58:32 +0900

    프로토타입 스코프 빈 테스트

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,47 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        // 프로토타입 빈은 프로토타입 빈을 조회한 클라이언트가 관리해야 한다
+        // => 종료 메서드에 대한 호출도 클라이언트가 직접 해야한다.
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+
+        ac.close(); // => destroy 출력 안 함
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
