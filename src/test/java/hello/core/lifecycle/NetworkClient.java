81b276fe38a8bb04e7aa38471ab3f5389fda79d9
syk050 <syk050@naver.com>
2022-10-24 13:08:28 +0900

    빈 생명주기 콜백

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,31 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("Connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + ", message: " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("disconnect: " + url);
+    }
+}
