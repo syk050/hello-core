4b884fd30a8e46b614441d24f8d7702d1b139d5d
syk050 <syk050@naver.com>
2022-10-24 13:13:22 +0900

    빈 생명주기 콜백 - 인터페이스

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,15 +1,14 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient {
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -27,5 +26,22 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("disconnect: " + url);
+    }
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 의존관계 주입이 끝나면 호출 PDF: 8- 5p
+        System.out.println("NetworkClient.afterPropertiesSet");
+
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+
+        disconnect();
     }
 }
