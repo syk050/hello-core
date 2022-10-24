964eb85fa9f90e9492845510f9d93a5eac0475a7
syk050 <syk050@naver.com>
2022-10-24 14:41:53 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,7 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -13,26 +14,28 @@ public class SingletonWithPrototypeTest1 {
     @Test
     void singletonClientUsePrototype() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
-        
+
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
-        
+
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        // 여기가 문제
-        // prototypeBean이 새로 생성된게 아닌 
+        // 여기가 문제 PDF: 8- 11p
+        // prototypeBean이 새로 생성된게 아닌
         // clientBean1이 생성한 prototypeBean을 공유한다
-        assertThat(count2).isEqualTo(2);
+        //assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
+
+    @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
-        
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
         public int logic() {
+            // 내부에서는 스프링 컨테이너를 통해 해당 빈을 찾아서 반환한다. (DL)
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
